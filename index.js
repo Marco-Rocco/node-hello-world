a2c2be808bc1da714d72450f752de06cb7af3864
Marco-Rocco <marcorocco28@icloud.com>
2025-03-25 16:55:39 +0100

    feat: interpolation of words so that they show up separetely in terminal

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let message = ''
 
 for (let i = 2; i < process.argv.length; i++) {
-    message = process.argv[i];
+    message += `${process.argv[i]} ` ;
 }
 
+// console.log(process.argv)
 console.log(message);
-
-console.log(process.argv);
